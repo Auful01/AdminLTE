6c126609d69e0cd59df55ac04935db0b5943bf26
Auful01 <auful@gmail.com>
2020-07-02 08:57:35 +0700

    first commit

---
+++ resources/views/items/create.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte.master')
+
+<!-- @section('content')
+<h2>Ini halaman create</h2>
+@endsection -->
+
+@push('scripts')
+<script src="{{asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('adminlte/dist/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('adminlte/dist/js/demo.js')}}"></script>
+
+<!-- <script>
+    var wrappers = document.getElementsByClassName("wrappers");
+    var items = ["ini","contoh"];
+
+    console.log(items);
+</script> -->
+@endpush
---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,6 @@
+@extends('adminlte.master')
+
+@section('content')
+    <h1>Coba Gaes</h1>
+    
+@endsection
